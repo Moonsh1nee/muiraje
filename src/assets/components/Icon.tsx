78f7cf17f4abfddc54e879859071b0dd8648f9ea
Moonsh1ne <moonsh1ne.vlad@yandex.ru>
2025-05-19 16:56:38 +0300

    step 3 catalog

--- src/assets/components/Icon.tsx
+++ src/assets/components/Icon.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import styles from '@/assets/styles/components/Icon.module.scss';
 
-export type IconName = 'computer';
+export type IconName = 'computer' | 'btnCross' | 'btnRoll' | 'btnFullScreen';
 
 interface IconProps {
   name: IconName;
--- src/assets/components/catalog/Header.tsx
+++ src/assets/components/catalog/Header.tsx
@@ -19,6 +19,29 @@ export const Header = ({ nav }: HeaderProps) => {
           {nav && <div className={styles.logoNav}>{nav}</div>}
         </div>
       </div>
+      <div className={styles.headerButtons}>
+        <button className={styles.headerButton}>
+          <div className={styles.headerButtonTop}>
+            <div className={styles.headerButtonContent}>
+              <Icon name="btnRoll" width={21} height={6} />
+            </div>
+          </div>
+        </button>
+        <button className={styles.headerButton}>
+          <div className={styles.headerButtonTop}>
+            <div className={styles.headerButtonContent}>
+              <Icon name="btnFullScreen" width={25} height={25} />
+            </div>
+          </div>
+        </button>
+        <button className={styles.headerButton}>
+          <div className={styles.headerButtonTop}>
+            <div className={styles.headerButtonContent}>
+              <Icon name="btnCross" width={25} height={24} />
+            </div>
+          </div>
+        </button>
+      </div>
     </header>
   );
 };
